f166ecbd2ad6b78bb19bda990d73915f0ba278ae
Ivan Herhi <ionherhi@gmail.com>
2025-03-03 19:07:35 +0100

    DOM-01

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const body = document.body;
+const nodeList = body.querySelectorAll('.pupulation');
+/* Nodelist  типу масив*/
+const array = Array.from(nodeList);
+
+let total = array.reduce((accum, item) => {
+  return accum + +item.textContent;
+},0);
+
+let average = total / array.length;
+
+let formattedTotal = total.toLocaleString();
+let formattedAverage = average.toLocaleString();
+
+
+ducument.querySelector('.total-population').textContent = formattedTotal;
+ducument.querySelector('.average-population').textContent = formattedAverage;
+
